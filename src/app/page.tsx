d8b97b2a6690198171a20b1d9f8469334c26c47c
BuildTools <drew.escorcia@gmail.com>
2024-04-20 21:10:15 -0400

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,12 +29,12 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-center">
         <a
-          href="https://www.facebook.com/Ax2Tech"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+            href="mailto:info@ax2tech.com"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
             Inquiries{" "}
@@ -42,15 +42,34 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            All of your tech needs, a phone call away 
+          <p className="m-0 max-w-[30ch] text-md opacity-50">
+            Email us info@ax2tech.com
           </p>
         </a>
 
         <a
-          href="https://www.facebook.com/Ax2Tech"
-          target="_blank"
-          rel="noopener noreferrer"
+            href="tel:+15163109172"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Call us{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-md opacity-50">
+            (516)-310-9172<br/>
+            (929)-485-1690
+          </p>
+        </a>
+
+        <a
+            href="https://www.facebook.com/Ax2Tech"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
             Facebook{" "}
@@ -58,27 +77,9 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Visit our social media!
+          <p className="m-0 max-w-[30ch] text-md opacity-50">
+            Visit us on Facebook!
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         
         </a>
       </div>
     </main>
